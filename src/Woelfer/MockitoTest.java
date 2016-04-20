d894cac9919211ab96eae495e070aa4ffb8d8c8a
Martin Wölfer <mwoelfer01.student@tgm.ac.at>
2016-04-20 09:05:10 +0200

    added test for stubbing

--- src/Woelfer/MockitoTest.java
+++ src/Woelfer/MockitoTest.java
@@ -2,6 +2,7 @@ package Woelfer;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.After;
@@ -22,13 +23,37 @@ public class MockitoTest {
 	@Test
 	public void testBehaviour() {
 		List mockedList = mock(List.class);
-		 //using mock object
-		 mockedList.add("one");
-		 mockedList.clear();
+		//using mock object
+		mockedList.add("one");
+		mockedList.clear();
+		 
+		//verification
+		verify(mockedList).add("one");
+		verify(mockedList).clear();
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testStubbing(){
+		//You can mock concrete classes, not just interfaces
+		 LinkedList mockedList = mock(LinkedList.class);
 
-		 //verification
-		 verify(mockedList).add("one");
-		 verify(mockedList).clear();
+		 //stubbing
+		 when(mockedList.get(0)).thenReturn("first");
+		 when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+		 //following prints "first"
+		 System.out.println(mockedList.get(0));
+
+		 //following throws runtime exception
+		 System.out.println(mockedList.get(1));
+
+		 //following prints "null" because get(999) was not stubbed
+		 System.out.println(mockedList.get(999));
+
+		 //Although it is possible to verify a stubbed invocation, usually it's just redundant
+		 //If your code cares what get(0) returns, then something else breaks (often even before verify() gets executed).
+		 //If your code doesn't care what get(0) returns, then it should not be stubbed. Not convinced? See here.
+		 verify(mockedList).get(0);
 	}
 
 }
