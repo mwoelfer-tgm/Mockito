bf2171562c1fa35aacc32ea8bc3bac1833168ae1
Martin Wölfer <mwoelfer01@student.tgm.ac.at>
2016-04-26 18:42:49 +0200

    Added class for the for shorthand invocations

---
+++ src/Woelfer/annotationTest.java
@@ -0,0 +1,15 @@
+package Woelfer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.mockito.Mock;
+
+public class annotationTest {
+
+    @Mock private String test1;
+    @Mock private Integer test2;
+    @Mock private LinkedList test3;
+
+    private List manager;
+}
